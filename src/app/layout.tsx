1e7bbfafab29521a54d0f874d52aab52777323ed
mjahidhasand4 <mjahidhasand4@gmail.com>
2024-01-20 21:50:50 +0600

    🚀 Initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Discord",
+};
+
+const RootLayout = ({ children }: Readonly<{ children: React.ReactNode }>) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default RootLayout;
